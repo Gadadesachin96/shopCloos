15ee4c0f66dda5048305d093361f4f7885d99688
sachin <gadadesachin51@gmail.com>
2024-07-13 23:40:16 +0530

    commit for deploy

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  build: {
+    plugins: [react()],
+    rollupOptions: {
+      external: ['react-router-dom']
+    }
+  }
+  
+  
 })
